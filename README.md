c465699cca03384138cb1424bd6a5b48fa1d6395
icandebug <icandebug@gmail.com>
2021-02-06 10:27:30 +0800

    单页面简易路由

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
 # web-learning
 前端小学生，长路漫漫，唯键作伴。  
 
-我要悄悄学**前端**，然后惊艳所有人。  
+我们要悄悄学**前端**，然后惊艳所有人。  
 
-# markdown sytax
-*斜体*  
-**加粗** 
-***加粗并斜体***
-> 这是一个blockquotes  
 
-##列表
+##list
 - html5
 - css3
 - javascript
 - nodejs
 - vue
-<strong>来吧</strong>
--- node/app2.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var url = require('url');
-var util = require('util');
- 
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.end(util.inspect(url.parse(req.url, true)));
-}).listen(3000);
