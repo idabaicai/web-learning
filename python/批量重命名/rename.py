65b0bdd43d082a869f4f4c042869b0f74acb199a
idabaicai <icandebug@gmail.com>
2021-04-27 09:36:55 +0800

    feat: 删除多余代码

--- python/批量重命名/rename.py
+++ python/批量重命名/rename.py
@@ -1,9 +1,9 @@
 import os
 
-path = r'E:\web前端\228 - ustbhuangyi-Vue.js 源码全方位深入解析 （含 Vue3.0 源码分析）\第6章 编译（上）'
+path = r'E:\web前端\228 - XXXXXXXXXXX'
 fileList = os.listdir(path)
 
-replace = '【更多it资源微信610060008】'
+replace = '【more web learning source】'
 # 需要删除的长度
 length = len(replace)
 print(length)
