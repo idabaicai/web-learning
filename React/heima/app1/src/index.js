b773685679e886c40cd62c18fda73946987ee507
idabaicai <icandebug@gmail.com>
2021-05-08 10:56:49 +0800

    feat: updateing the rendered element

--- React/heima/app1/src/index.js
+++ React/heima/app1/src/index.js
@@ -1,24 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const user = {
-  firstName: 'da',
-  lastName: 'bai'
-}
-function formatName(user) {
-  if(user) {
-    return `${user.firstName} ${user.lastName}`
-  }
-  return <h1> Hello, Stranger </h1>
-}
 
-const element = (
-  <div>
-    <h1 className="title"> hello {formatName(user)} </h1>
-    <h2>Good to see you here</h2>
-  </div>
-)
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-)
+function tick() {
+  const element = (
+    <div>
+      <h3> It is {new Date().toLocaleTimeString()} </h3>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'))
+}
+setInterval(tick, 1000)
