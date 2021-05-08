606636a5b899174d3aff00e97277b26d9ab53207
idabaicai <icandebug@gmail.com>
2021-05-08 10:37:35 +0800

    feat: using javascript expression

--- React/heima/app1/src/index.js
+++ React/heima/app1/src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const title = React.createElement('h1', null, 'I am h1')
-ReactDOM.render(title, document.getElementById('root'))
+const user = {
+  firstName: 'da',
+  lastName: 'bai'
+}
+function formatName(user) {
+  return `${user.firstName} ${user.lastName}`
+}
+
+const element = (
+  <h1> hello {formatName(user)} </h1>
+)
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+)
