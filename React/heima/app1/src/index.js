538769c59a394ae66061d70257318c34d68d66e8
idabaicai <icandebug@gmail.com>
2021-05-08 14:49:40 +0800

    feat: extracting components

--- React/heima/app1/src/index.js
+++ React/heima/app1/src/index.js
@@ -36,13 +36,63 @@ class Welcome1 extends React.Component {
     return <h1>Hello, {this.props.name}</h1>
   }
 }
+
+/**
+ * Extrating Components
+ */
+const user = {
+  name: 'dabai',
+  avatar: 'https://cdn.jsdelivr.net/gh/idabaicai/ImageBase@master/2021/avatar.2641s37p23hc.jpg',
+}
+function Avatar(props) {
+  return (
+    <img className="avatar"
+      src={props.user.avatar}
+      alt={props.user.name}
+     />
+  )
+}
+
+function UserInfo(props) {
+  return (
+    <div className="user-info">
+      <Avatar user={props.user} />
+      <div className="username">
+        {props.user.name}
+      </div>
+    </div>
+  )
+}
+
+function Comment(props) {
+  return (
+    <div className="comment">
+    <UserInfo user={user} />
+      <div className="text">
+        {props.text}
+      </div>
+      <div className="data">
+        {props.date}
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="dabai" />
+      <Welcome1 name="dabai1" />
+      <Comment text="react is also good" user={user} />
+    </div>
+  )
+}
 ReactDOM.render(
   <div>
     <ul>
       {listItems}
     </ul>
-    <Welcome name="dabai" />
-    <Welcome1 name="weizhangdongwu" />
+    <App />
   </div>,
   document.getElementById('root')
 )
