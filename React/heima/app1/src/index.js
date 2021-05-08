e347d9d0da59e88e84a92f910913fd1dfa3bb734
idabaicai <icandebug@gmail.com>
2021-05-08 11:14:04 +0800

    feat: list and keys

--- React/heima/app1/src/index.js
+++ React/heima/app1/src/index.js
@@ -2,12 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-function tick() {
-  const element = (
-    <div>
-      <h3> It is {new Date().toLocaleTimeString()} </h3>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'))
-}
-setInterval(tick, 1000)
+// function tick() {
+//   const element = (
+//     <div>
+//       <h3> It is {new Date().toLocaleTimeString()} </h3>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'))
+// }
+// setInterval(tick, 1000)
+
+const numbers = [1, 2, 3, 4, 5]
+
+const listItems = numbers.map(item => {
+  return <li key={item}>{item}</li>
+})
+console.log(listItems)
+ReactDOM.render(
+  <ul>
+    {listItems}
+  </ul>,
+  document.getElementById('root')
+)
