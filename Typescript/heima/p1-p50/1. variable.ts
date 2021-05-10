d57f2933732bc6450f25207e6f982d39f11b0cc2
idabaicai <icandebug@gmail.com>
2021-05-10 17:41:32 +0800

    feat: array

--- Typescript/heima/p1-p50/1. variable.ts
+++ Typescript/heima/p1-p50/1. variable.ts
@@ -7,6 +7,9 @@ const hasHouse: boolean = false
 const undef: undefined = undefined
 const car: null = null
 
+const arr:Array<number> = [1, 3, 5]
+
+// 对象数据类型
 interface User {
   name: string,
   id: number,
@@ -20,3 +23,7 @@ const user1:User = {
 
 console.log(age, uname)
 console.log('user1', user1)
+
+arr.forEach(item => {
+  console.log(item)
+})
