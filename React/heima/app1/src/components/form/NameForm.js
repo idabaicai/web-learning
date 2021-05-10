f0b154d80704ce69543f8e566e1cab91a01e7ef8
idabaicai <icandebug@gmail.com>
2021-05-10 10:59:59 +0800

    feat: the textarea tag

--- React/heima/app1/src/components/form/NameForm.js
+++ React/heima/app1/src/components/form/NameForm.js
@@ -4,18 +4,25 @@ class NameForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      value: ''
+      value: '',
+      textValue: ''
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleTextChange = this.handleTextChange.bind(this)
   }
   handleSubmit(event) {
-    console.log(this.state.value)
+    console.log(this.state)
     event.preventDefault()
   }
   handleChange(event) {
     this.setState({
       value: event.target.value
+    })
+  }
+  handleTextChange(e) {
+    this.setState({
+      textValue: e.target.value
     })
   }
   render() {
@@ -29,6 +36,15 @@ class NameForm extends React.Component {
             onChange={this.handleChange}
             />
         </label>
+        <br />
+        <label>
+        TextArea:
+          <textarea
+            value={this.state.textValue}
+            onChange={this.handleTextChange}
+          ></textarea>
+        </label>
+        <br />
         <input type="submit" value="submit" />
       </form>
     )
