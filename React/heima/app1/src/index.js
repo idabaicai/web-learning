7bf5c8feec7c2a70f392e7ed2cac709e0bc7e9d4
idabaicai <icandebug@gmail.com>
2021-05-12 10:31:20 +0800

    feat: lifecycle

--- React/heima/app1/src/index.js
+++ React/heima/app1/src/index.js
@@ -4,14 +4,39 @@ import ReactDOM from 'react-dom'
 import PropsTypes from 'prop-types'
 
 class ListDemo extends React.Component {
+  constructor(props) {
+    super(props)
+    console.warn('constructor')
+    this.state = {
+      count: 1
+    }
+  }
+  handleClick() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
   render() {
+    console.warn('render')
     const list = this.props.list
     const li = list.map(item => (<li key={item}>item: {item}</li>))
     return (
-      <ul>
-        <li />
-      </ul>
+      <div>
+        <ul>
+          {li}
+        </ul>
+        <div className="count">
+          <p>{this.state.count}</p>
+          <button onClick={() => this.handleClick()}>add</button>
+        </div>
+      </div>
     )
+  }
+  componentDidMount() {
+    console.warn('componentDidMount');
+  }
+  componentDidUpdate() {
+    console.warn('componentDidUpdate')
   }
 }
 ListDemo.propTypes = {
@@ -21,8 +46,8 @@ ListDemo.propTypes = {
 function App() {
   return (
     <div>
-        {/* <List list={['vue', 'react']} /> */}
-        <ListDemo list={1} />
+        <ListDemo list={['vue', 'react']} />
+        {/* <ListDemo list={1} /> */}
     </div>
   )
 }
