f2f36e601082efff06ff1040e425f411d3157ae2
idabaicai <icandebug@gmail.com>
2021-05-14 09:12:31 +0800

    feat: higher order components

--- React/heima/app1/src/index.js
+++ React/heima/app1/src/index.js
@@ -1,85 +1,50 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom'
-import avatar from './assets/avatar.jpg'
 
-class Mouse extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleMouseMove = this.handleMouseMove.bind(this)
-    this.state = {
-      x: 0,
-      y: 0
+
+/**
+ * Higher-order components
+ * @param { Component } WrappedComponent 
+ * @returns 
+ */
+function WithMouse(WrappedComponent) {
+  class Mouse extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        x: 0,
+        y: 0
+      }
+    }
+    handleMouseMove(e) {
+      this.setState({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+    render() {
+      return (
+        <div style={{height:'100vh'}} onMouseMove = {(e) => this.handleMouseMove(e)}>
+          <WrappedComponent {...this.state} />
+        </div>
+      )
     }
   }
-  handleMouseMove(e) {
-    this.setState({
-      x: e.clientX,
-      y: e.clientY
-    })
-  }
-  render() {
-    return (
-      <div style={{height: '50vh'}} onMouseMove={this.handleMouseMove}>
-        {this.props.render(this.state)}
-      </div>
-    )
-  }
+  return Mouse
 }
 
-class Cat extends React.Component {
-  render() {
-    const mouse = this.props.mouse
-    return (
-      <div>
-        {/* <img src={avatar} alt="cat" style={{position: 'absolute', left: mouse.x, top: mouse.y}} /> */}
-        <img src={avatar} alt="cat" style={{width: '100px', height: '100px',position: 'absolute', left: mouse.x - 50, top: mouse.y - 50}} />
-      </div>
-    )
-  }
-}
+const Point = (props) => (
+  <div>
+    the popint is ({props.x}, {props.y}).
+  </div>
+)
 
-class DisPlay extends React.Component {
-  render() {
-    const mouse = this.props.mouse
-    return (
-      <div>
-        <h2>The poit is ({mouse.x},{mouse.y})</h2>
-      </div>
-    )
-  }
-}
-
-class MouseTracker extends React.Component {
-  render() {
-    return (
-      <div>
-        <Mouse render={mouse => (
-          <Cat mouse={mouse} />
-        )} 
-        />
-      </div>
-    )
-  }
-}
-
-class MouseTracker1 extends React.Component {
-  render() {
-    return (
-      <div>
-        <Mouse render={ mouse => (
-          <DisPlay mouse={mouse} />
-        )} />
-      </div>
-    )
-  }
-}
+const WithMousePoint = WithMouse(Point)
 
 function App() {
   return (
     <div>
-      <MouseTracker />
-      {/* <MouseTracker1 /> */}
+      <WithMousePoint />
     </div>
   )
 }
