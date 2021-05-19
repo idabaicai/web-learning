8f7eed34feb0057ce93a2a8825fce1fb18bb4724
idabaicai <icandebug@gmail.com>
2021-05-19 17:47:52 +0800

    feat: setState update state is async

--- React/heima/app1/src/index.js
+++ React/heima/app1/src/index.js
@@ -1,54 +1,42 @@
+// 异步更新状态
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
-/**
- * Higher-order components
- * @param { Component } WrappedComponent 
- * @returns 
- */
-function WithMouse(WrappedComponent) {
-  class Mouse extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        x: 0,
-        y: 0
-      }
-    }
-    handleMouseMove(e) {
-      this.setState({
-        x: e.clientX,
-        y: e.clientY
-      })
-    }
-    render() {
-      return (
-        <div style={{height:'100vh'}} onMouseMove = {(e) => this.handleMouseMove(e)}>
-          <WrappedComponent {...this.state} {...this.props} />
-        </div>
-      )
+class Demo extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 1
     }
   }
-  return Mouse
+  
+  handleClick() {
+    this.setState({
+      count: this.state.count + 1
+    })
+    console.log('after first update', this.state.count)
+    
+    this.setState({
+      count: this.state.count + 1
+    })
+    console.log('after sencond update',this.state.count)
+  }
+  render() {
+    console.log('render')
+    return (
+      <div>
+        <p> count: {this.state.count} </p>
+        <button onClick={() => this.handleClick()}>add</button>
+      </div>
+    )
+  }
 }
-
-const Point = (props) => {
-  return (
-    <div>
-      the popint is ({props.x}, {props.y}).
-    </div>
-  )
-}
-
-const WithMousePoint = WithMouse(Point)
 
 function App() {
   return (
     <div>
-      <WithMousePoint name="HOC" />
+      <Demo />
     </div>
   )
 }
-
 ReactDOM.render(<App />, document.getElementById('root'))
