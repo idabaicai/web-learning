be2239fd5c041f32903c9e12224d4f4998699a2f
idabaicai <icandebug@gmail.com>
2021-05-21 21:29:43 +0800

    feat: useEffet

--- React/heima/app1/src/index.js
+++ React/heima/app1/src/index.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 function Example() {
   // Declare a new state
   const [count, setCount] = useState(0);
+  // const [todos, setTodos] = useState({text: 'learn hooks'})
+  
+  // Similar to componentDidMount and componentDidUpdate
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  })
   return (
     <div>
       <p> You clicked {count} times </p>
