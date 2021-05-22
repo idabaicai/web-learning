5d67ee881aff435c17a5b9b7f1a234dd2224e719
idabaicai <icandebug@gmail.com>
2021-05-22 15:20:23 +0800

    feat: useEffet 中使用async 和 await

--- React/shop/app-ts/src/App.tsx
+++ React/shop/app-ts/src/App.tsx
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import robots from './mockdata/robots.json'
 import Robots from './components/Robots'
 import styles from './App.module.css'
 import logo from './assets/images/logo.svg'
 import ShoppingCart from './components/ShoppingCart'
 
-interface Props {
+const App: React.FC = () =>  {
 
-}
-interface State {
-  robotGallery: any // 后端返回的数据一般用 any
-}
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      robotGallery: []
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     this.setState({
+  //       robotGallery: data
+  //     })
+  //   })
+  // }
+  const [count, setCount] = useState<number>(0)
+  const [robotGallery, setRobotGallery] = useState<any>([])
+
+  useEffect(() => { // 相当于 componentDidUpdate
+    document.title = `click ${count} times`
+  }, [count])
+
+  useEffect(() => { // 第二个参数传入 []， 相当与 componentDidMount
+    // 在 useEffect 中使用 async 和 await
+    const fetchData = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await res.json()
+      setRobotGallery(data)
     }
-  }
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => {
-      this.setState({
-        robotGallery: data
-      })
-    })
-  }
-  render() {
+    fetchData()
+  }, [])
     return (
       <div className={styles.app}>
+        <div>
+          <p>点击了 {count} times</p>
+          <button onClick={() => setCount(count + 1)}>click</button>
+        </div>
         {/* header start */}
         <div className={styles.appHeader}>
           <img src={logo} alt="logo" className={styles.appLogo} />
@@ -38,11 +46,10 @@ class App extends React.Component<Props, State> {
         {/* header end */}
         <ShoppingCart />
         <div className={styles.robotList}>
-          {this.state.robotGallery.map(item => <Robots key={item.id} id={item.id} name={item.name} email={item.email} />)}
+          {robotGallery.map(item => <Robots key={item.id} id={item.id} name={item.name} email={item.email} />)}
         </div>
       </div>
     );
-  }
 }
 
 export default App;
