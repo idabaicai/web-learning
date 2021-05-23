5e5e61516e41ebbbd7badcb186b4e1db17638f90
idabaicai <icandebug@gmail.com>
2021-05-23 21:00:13 +0800

    feat: 基础柱状图

--- html5+css3+h5/ECharts/javascript/index.js
+++ html5+css3+h5/ECharts/javascript/index.js
@@ -7,3 +7,28 @@ function showTime() {
 }
 
 setInterval(showTime, 1000)
+
+const echartBar1 = echarts.init(document.getElementById('bar1'))
+
+const option = {
+  // 指定图表的配置项和数据
+    title: {
+        text: '柱状图就业行业',
+        textStyle: {
+          color: '#fff',
+        },
+        textAlign: 'auto'
+    },
+    tooltip: {},
+    xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    },
+    yAxis: {},
+    series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+};
+
+echartBar1.setOption(option)
