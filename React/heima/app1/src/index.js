3cf45bd9b222975ab7ec5697e4d8cc1881e94b8f
FUMAAI\liuhui <icandebug@foxmail.com>
2021-05-26 10:32:30 +0800

    feat: mobx usage

--- React/heima/app1/src/index.js
+++ React/heima/app1/src/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
+import { makeAutoObservable } from 'mobx'
+import { observer } from 'mobx-react'
+
 function Example() {
   // Declare a new state
   const [count, setCount] = useState(0);
@@ -18,4 +21,27 @@ function Example() {
   )
 }
 
-ReactDOM.render(<Example />, document.getElementById('root'))
+class Timer {
+  secondsPass = 0;
+  constructor() {
+    makeAutoObservable(this)
+  }
+
+  increase() {
+    this.secondsPass += 1;
+  }
+  reset() {
+    this.secondsPass = 0;
+  }
+}
+const myTimer = new Timer()
+const TimerView = observer(({ timer }) => (
+  <button onClick={() => this.reset()}> Seconds passed: {timer.secondsPass} </button>
+))
+
+// ReactDOM.render(<Example />, document.getElementById('root'))
+ReactDOM.render(<TimerView timer={myTimer} />, document.getElementById('root'))
+
+setInterval(() => {
+  myTimer.increase()
+}, 1000);
