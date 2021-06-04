c878918663b1d78b765c7840156a10ce78b5177c
idabaicai <icandebug@gmail.com>
2021-06-04 10:59:34 +0800

    feat: 删除多余代码

--- vue/vue-project/vue-admin-js/src/views/Home.vue
+++ vue/vue-project/vue-admin-js/src/views/Home.vue
@@ -1,18 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>home</h2>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
