c084fa71d607b6dcf6da8b6496aec90317850d11
idabaicai <icandebug@gmail.com>
2021-06-04 09:51:14 +0800

    feat: 封装 axios

--- vue/vue-project/vue-admin/src/utils/request.ts
+++ vue/vue-project/vue-admin/src/utils/request.ts
@@ -1,6 +1,43 @@
 import axios from 'axios'
 import { Message } from 'element-ui';
+import store from '@/store'
 
 const instance = axios.create({
-  baseURL: process.env.B
+  baseURL: process.env.BASE_URL,
+  timeout: 5000,
+  // headers: { 'c-header': 'value' }
 })
+
+// 请求拦截
+instance.interceptors.request.use( config => {
+  config.headers['C-Token'] = 'sesxvxeh'
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+// 响应拦截
+
+instance.interceptors.response.use(response => {
+  console.log(response.data)
+  if(response.data.code !== 200) {
+    Message({
+      message: response.data.message || 'Error',
+      type: 'error',
+      duration: 3 * 1000
+    })
+  } else {
+    return response
+  }
+  return response
+}, error => {
+  console.log(error)
+  Message({
+    message: error.message || 'Error',
+    type: 'error',
+    duration: 3 * 1000
+  })
+  return Promise.reject(error)
+})
+
+export default instance
