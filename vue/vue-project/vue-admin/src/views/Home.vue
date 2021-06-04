5688c07b2e3a94769bef6f88f78229b91f9ebbd2
idabaicai <icandebug@gmail.com>
2021-06-04 09:06:30 +0800

    feat: 测试eslint

--- vue/vue-project/vue-admin/src/views/Home.vue
+++ vue/vue-project/vue-admin/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    home
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-
 export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      name: 'we'
+    }
   }
 })
 </script>
