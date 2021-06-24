5e20aa55350bf185346130477e1ad91af32ad702
idabaicai <icandebug@gmail.com>
2021-06-24 13:43:46 +0800

    feat: add two number

--- LeetCode/1.1-10/2.add_two_number/2.add_two_number.js
+++ LeetCode/1.1-10/2.add_two_number/2.add_two_number.js
@@ -3,14 +3,36 @@ var ListNode = /** @class */ (function () {
         this.val = val === undefined ? 0 : val;
         this.next = next === undefined ? null : next;
     }
-    ListNode.prototype.append = function (val) {
-        this.next = new ListNode(val, null);
-    };
     return ListNode;
 }());
-var listNode = new ListNode(3);
-listNode.append(2);
-listNode.append(6);
-listNode.append(3);
-listNode.append(7);
-console.log(listNode);
+function addTwoNumbers(l1, l2) {
+    var l3 = new ListNode();
+    var len1 = 0; // l1 长度
+    var len2 = 0; // l2 长度
+    var l11 = l1;
+    var l22 = l2;
+    // 计算 ListNode1 和 ListNode2 长度 
+    while (l11.next !== null) {
+        len1++;
+        l11 = l11.next;
+    }
+    while (l22.next !== null) {
+        len2++;
+        l22 = l22.next;
+    }
+    console.log('li and l2', l1, l2);
+    return null;
+}
+var listNode2 = new ListNode(2);
+var listNode4 = new ListNode(4);
+var listNode3 = new ListNode(3);
+// 连接链表
+listNode2.next = listNode4;
+listNode4.next = listNode3;
+var listNode5 = new ListNode(5);
+var listNode6 = new ListNode(6);
+var listNode7 = new ListNode(7);
+listNode5.next = listNode6;
+listNode6.next = listNode7;
+console.log(listNode2);
+console.log(addTwoNumbers(listNode2, listNode5));
--- LeetCode/1.1-10/2.add_two_number/2.add_two_number.ts
+++ LeetCode/1.1-10/2.add_two_number/2.add_two_number.ts
@@ -6,17 +6,43 @@ class ListNode {
     this.val = val === undefined ? 0 : val;
     this.next = next === undefined ? null : next;
   }
-  append(val:number):void {
-    this.next = new ListNode(val, null)
-  }
 }
 
-const listNode = new ListNode(3);
-listNode.append(2);
-listNode.append(6);
-listNode.append(3);
-listNode.append(7);
+function addTwoNumbers(l1:ListNode | null, l2: ListNode | null): ListNode | null {
+  const l3 = new ListNode();
+  let len1 = 0; // l1 长度
+  let len2 = 0; // l2 长度
+  let l11 = l1;
+  let l22 = l2;
+  // 计算 ListNode1 和 ListNode2 长度 
+  while(l11.next !== null) {
+    len1++;
+    l11 = l11.next;
+  }
+  while(l22.next !== null) {
+    len2++;
+    l22 = l22.next;
+  }
+  console.log('li and l2',l1, l2);
+  return null;
+}
+
+const listNode2 = new ListNode(2);
+const listNode4 = new ListNode(4);
+const listNode3 = new ListNode(3);
+// 连接链表
+listNode2.next = listNode4;
+listNode4.next = listNode3;
 
 
-console.log(listNode);
+const listNode5 = new ListNode(5);
+const listNode6 = new ListNode(6);
+const listNode7 = new ListNode(7);
+
+listNode5.next = listNode6;
+listNode6.next = listNode7;
+
+console.log(listNode2);
+console.log(addTwoNumbers(listNode2, listNode5));
+
 
