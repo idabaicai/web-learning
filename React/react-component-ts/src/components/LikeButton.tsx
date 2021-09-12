89a32c5959a43aca0273810455f148a704607493
idabaicai <icandebug@gmail.com>
2021-09-12 18:00:43 +0800

    learn: useEffect Hooks

--- React/react-component-ts/src/components/LikeButton.tsx
+++ React/react-component-ts/src/components/LikeButton.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const LikeButton: React.FC = () => {
   // const [obj, setObj] = useState({like: 0, on: true});
   const [like, setLike] = useState(0);
   const [on, setOn ] = useState(true);
+
+  // similar to componentDidMount and componentDidupdate:
+  useEffect(() => {
+    document.title = `You click button ${like} times`;
+  }, [like]); // Only re-run the effet if like changes
   return (
     <div>
       <button onClick={() => {
