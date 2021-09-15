f96444f2cc671858cb8f1593b8970e90e5588f66
idabaicai <icandebug@gmail.com>
2021-09-15 22:38:54 +0800

    style: 添加 normalize.scss 文件

--- React/meta-ui/src/index.tsx
+++ React/meta-ui/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
 
 ReactDOM.render(
   <React.StrictMode>
