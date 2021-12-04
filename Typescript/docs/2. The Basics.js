67a1b215f43ac0cc891c540ffa8833e27f335273
idabaicai <icandebug@gmail.com>
2021-12-04 08:30:22 +0800

    learn: unknow type

--- Typescript/docs/2. The Basics.js
+++ Typescript/docs/2. The Basics.js
@@ -1,2 +1,14 @@
-var msg = 'hello';
-console.log(msg);
+// Type 'unknown' is not assignable to type 'number'
+// const aNumber:number = maybe;
+if (maybe === true) {
+    //  TS knowns that maybe is a boolean now
+    var aBoolean = maybe;
+    // Type 'boolean' is not assignable to type 'string'.
+    // const aString: string = maybe;
+}
+if (typeof maybe === 'string') {
+    // TS knowns that maybe is a 'string' now
+    var aString = maybe;
+    // Type 'string' is not assignable to type 'boolean'
+    // const aBoolean: boolean = maybe;
+}
--- Typescript/docs/2. The Basics.ts
+++ Typescript/docs/2. The Basics.ts
@@ -1,2 +1,20 @@
-const msg = 'hello' as string
-console.log(msg)
+
+//  Unknown
+declare const maybe: unknown;
+
+// Type 'unknown' is not assignable to type 'number'
+// const aNumber:number = maybe;
+
+if(maybe === true) {
+  //  TS knowns that maybe is a boolean now
+  const aBoolean: boolean = maybe;
+  // Type 'boolean' is not assignable to type 'string'.
+  // const aString: string = maybe;
+}
+
+if(typeof maybe === 'string') {
+  // TS knowns that maybe is a 'string' now
+  const aString: string = maybe;
+  // Type 'string' is not assignable to type 'boolean'
+  // const aBoolean: boolean = maybe;
+}
