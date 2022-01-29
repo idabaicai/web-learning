a6df02aba26f7e44a471f470c52312f1d070f0b3
idabaicai <icandebug@gmail.com>
2022-01-29 16:26:50 +0800

    learn: RN http request

--- React/RN/AppDemo/App.js
+++ React/RN/AppDemo/App.js
@@ -1,71 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 
-import {Button, Text, View, StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-const HomeScreen = ({navigation}) => (
-  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-    <Text style={{fontSize: 24}}>Home Screen</Text>
-    <Button
-      title="Go to Details"
-      onPress={() => {
-        navigation.navigate('Detail', {
-          itemId: 68,
-          msg: 'Hello detail',
-        });
-      }}
-    />
-  </View>
-);
-
-const DetailScreen = ({route, navigation}) => {
-  const {itemId, msg} = route.params;
+const FetchList = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const url = 'https://reactnative.dev/movies.json';
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(res => setData(res.movies))
+      .catch(err => {
+        console.error(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      }, []);
+  });
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{marginBottom: 12}}>Detail Page.</Text>
-      <Text>Id is {itemId}</Text>
-      <Text>Msg is {msg}</Text>
-      <View style={styles.btnWrap}>
-        <Button
-          title="Go to Details again"
-          onPress={() => {
-            navigation.push('Detail');
-          }}
+    <View>
+      <Text>FetchList</Text>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({item}) => (
+            <Text>
+              {item.title}: {item.releaseYear}
+            </Text>
+          )}
         />
-        <Button
-          style={styles.btn}
-          title="Go Back"
-          onPress={() => {
-            navigation.goBack();
-          }}
-        />
-      </View>
+      )}
     </View>
   );
 };
 
-const Stack = createNativeStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="HOME"
-          component={HomeScreen}
-          options={{title: 'Home title'}}
-        />
-        <Stack.Screen name="Detail" component={DetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+const App = () => (
+  <View style={styles.container}>
+    <FetchList />
+  </View>
+);
 
 const styles = StyleSheet.create({
-  btnWrap: {
-    height: 100,
-    justifyContent: 'space-between',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
