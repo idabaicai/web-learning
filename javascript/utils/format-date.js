a2ad6c5c0f3c3386acb4025253d500c9d3934d1e
idabaicai <icandebug@gmail.com>
2022-07-16 11:39:26 +0800

    feat ： 修改备注

--- javascript/utils/format-date.js
+++ javascript/utils/format-date.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-07-16 10:56:50
  * @Author: liuhui
- * @LastEditTime: 2022-07-16 11:32:08
+ * @LastEditTime: 2022-07-16 11:39:11
  * @LastEditors: liuhui
  * @Description: 日期格式化
  */
@@ -18,7 +18,7 @@ function format(time, format = 'yyyy:MM:dd') {
   };
   const time_str = format.replace(/([yMdHms])+/g, (res, key) => {
     const value = timeObj[key];
-    // res: 匹配到 format 的长度
+    // res: 匹配到 format 的字符
     const len = res.length;
     return value.toString().padStart(len, '0');
   });
