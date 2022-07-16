7747f8cff6341bb6209544128ba0f57176785b46
idabaicai <icandebug@gmail.com>
2022-07-16 11:33:06 +0800

    feat: 新增格式化日期函数

---
+++ javascript/utils/format-date.js
@@ -0,0 +1,26 @@
+/*
+ * @Date: 2022-07-16 10:56:50
+ * @Author: liuhui
+ * @LastEditTime: 2022-07-16 11:32:08
+ * @LastEditors: liuhui
+ * @Description: 日期格式化
+ */
+
+function format(time, format = 'yyyy:MM:dd') {
+  const date = new Date(time);
+  const timeObj = {
+    y: date.getFullYear(),
+    M: date.getMonth() + 1,
+    d: date.getDate(),
+    H: date.getHours(),
+    m: date.getMinutes(),
+    s: date.getSeconds(),
+  };
+  const time_str = format.replace(/([yMdHms])+/g, (res, key) => {
+    const value = timeObj[key];
+    // res: 匹配到 format 的长度
+    const len = res.length;
+    return value.toString().padStart(len, '0');
+  });
+  return time_str;
+}
